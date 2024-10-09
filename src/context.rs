166e583ad727f20759f736a18424c500e114bae7
Ruby Nealon <ruby@ruby.sh>
2024-10-09 19:16:09 +0900

    longer cache ttl for non-tmpl/content, better error logging

--- src/context.rs
+++ src/context.rs
@@ -1,25 +1,32 @@
-use std::{ffi::OsString, fs, path::PathBuf, sync::Mutex};
+use std::{
+    ffi::{OsStr, OsString},
+    fs,
+    path::PathBuf,
+    sync::Mutex,
+};
 
 use crate::{config::Config, templates::initialize_handlebars};
-use cached::{Cached as _, TimedSizedCache};
+use cached::stores::ExpiringSizedCache;
 use glob::glob;
 use handlebars::Handlebars;
-use log::debug;
+use log::{debug, error};
 use serde::Serialize;
 use serde_json::json;
 
 const MAX_FS_CACHE_ENTRIES: usize = 512;
-// TODO: allow granularity by file type, images should be way higher for example
-const MAX_FS_CACHE_TTL_SECONDS: u64 = 10;
-const MAX_DATA_CACHE_ENTRIES: usize = 512;
-const MAX_DATA_CACHE_TTL_SECONDS: u64 = 10;
+const MAX_FS_CACHE_LONG_TTL_MS: u64 = 14_400_000;
 
-#[derive(Debug)]
+const MAX_FS_CACHE_SHORT_TTL_EXTENSIONS: &[&str] = &["hbs", "html", "gmi", "md", "json"];
+const MAX_FS_CACHE_SHORT_TTL_MS: u64 = 10_000;
+
+const MAX_DATA_CACHE_ENTRIES: usize = 512;
+const MAX_DATA_CACHE_TTL_MS: u64 = 10_000;
+
 pub struct ServerContext {
     config: Config,
     handlebars: Mutex<Handlebars<'static>>,
-    fs_cache: Mutex<TimedSizedCache<OsString, Vec<u8>>>,
-    data_cache: Mutex<TimedSizedCache<OsString, serde_json::Value>>,
+    fs_cache: Mutex<ExpiringSizedCache<OsString, Vec<u8>>>,
+    data_cache: Mutex<ExpiringSizedCache<OsString, serde_json::Value>>,
 }
 
 #[derive(Debug)]
@@ -37,13 +44,13 @@ impl ServerContext {
         ServerContext {
             config: config,
             handlebars: Mutex::new(handlebars),
-            fs_cache: Mutex::new(TimedSizedCache::with_size_and_lifespan(
+            fs_cache: Mutex::new(ExpiringSizedCache::with_capacity(
+                MAX_FS_CACHE_LONG_TTL_MS,
                 MAX_FS_CACHE_ENTRIES,
-                MAX_FS_CACHE_TTL_SECONDS,
             )),
-            data_cache: Mutex::new(TimedSizedCache::with_size_and_lifespan(
+            data_cache: Mutex::new(ExpiringSizedCache::with_capacity(
+                MAX_DATA_CACHE_TTL_MS,
                 MAX_DATA_CACHE_ENTRIES,
-                MAX_DATA_CACHE_TTL_SECONDS,
             )),
         }
     }
@@ -70,7 +77,6 @@ impl ServerContext {
     fn register_handlebars_templates(&self) {
         let base_data_path = format!("{}/", self.config().partials_path());
 
-        // TODO: could probably have better debug logging for errors here
         for entry in
             glob(&format!("{}**/*.hbs", base_data_path)).expect("Failed to read data glob pattern")
         {
@@ -89,32 +95,75 @@ impl ServerContext {
                         Ok(data) => match std::str::from_utf8(&data) {
                             Ok(value) => {
                                 let mut handlebars = self.handlebars.lock().unwrap();
-                                let _ = handlebars.register_template_string(&partial_name, value);
+                                match handlebars.register_template_string(&partial_name, value) {
+                                    Ok(_) => {}
+                                    Err(err) => error!(
+                                        "ERROR registering handlebar partial {}: {}",
+                                        partial_name, err
+                                    ),
+                                }
                             }
-                            Err(_) => (),
+                            Err(err) => error!(
+                                "ERROR loading handlebar partial {} as UTF-8: {}",
+                                partial_name, err
+                            ),
                         },
-                        Err(_) => (),
+                        Err(err) => {
+                            error!(
+                                "ERROR loading handlebar partial file {}: {}",
+                                partial_name, err
+                            )
+                        }
                     }
                 }
-                Err(_) => {}
+                Err(err) => {
+                    error!("ERROR loading JSON files by glob: {:?}", err)
+                }
             }
         }
     }
 
     pub fn fs_read(&self, path_buf: PathBuf) -> Result<Vec<u8>, std::io::Error> {
         let cloned_path_buf = path_buf.clone();
-        let cache_key = cloned_path_buf.as_os_str();
+        let cache_key = cloned_path_buf.as_os_str().to_os_string();
         let mut fs_cache = self.fs_cache.lock().unwrap();
 
-        match fs_cache.cache_get(cache_key) {
+        match fs_cache.get(&cache_key) {
             Some(data) => {
                 debug!("fs cache hit: {:?}", cache_key);
                 Ok(data.to_vec())
             }
             None => match fs::read(path_buf) {
                 Ok(data) => {
-                    debug!("fs cache miss: {:?}", cache_key);
-                    fs_cache.cache_set(cache_key.into(), data.clone());
+                    if MAX_FS_CACHE_SHORT_TTL_EXTENSIONS.contains(
+                        &cloned_path_buf
+                            .extension()
+                            .unwrap_or(OsStr::new(""))
+                            .to_str()
+                            .unwrap_or(""),
+                    ) {
+                        debug!("fs cache miss (short ttl): {:?}", cache_key);
+                        match fs_cache.insert_ttl(
+                            cache_key.clone(),
+                            data.clone(),
+                            MAX_FS_CACHE_SHORT_TTL_MS,
+                        ) {
+                            Ok(_) => {}
+                            Err(err) => error!(
+                                "ERROR short-ttl fs cache insert for {:?}: {:?}",
+                                cache_key, err
+                            ),
+                        }
+                    } else {
+                        debug!("fs cache miss (long ttl): {:?}", cache_key);
+                        match fs_cache.insert(cache_key.clone(), data.clone()) {
+                            Ok(_) => {}
+                            Err(err) => error!(
+                                "ERROR long-ttl fs cache insert for {:?}: {:?}",
+                                cache_key, err
+                            ),
+                        }
+                    }
                     Ok(data)
                 }
                 Err(err) => Err(err),
@@ -127,7 +176,6 @@ impl ServerContext {
 
         let mut data = json!({});
 
-        // TODO: could probably have better debug logging for errors here
         for entry in
             glob(&format!("{}**/*.json", base_data_path)).expect("Failed to read data glob pattern")
         {
@@ -145,10 +193,14 @@ impl ServerContext {
                         Ok(value) => {
                             data.as_object_mut().unwrap().insert(data_key, value);
                         }
-                        Err(_) => {}
+                        Err(err) => {
+                            error!("ERROR reading data JSON file {}: {:?}", data_key, err)
+                        }
                     }
                 }
-                Err(_) => {}
+                Err(err) => {
+                    error!("ERROR loading JSON files by glob: {:?}", err)
+                }
             }
         }
 
@@ -157,10 +209,10 @@ impl ServerContext {
 
     fn data_read(&self, path_buf: PathBuf) -> Result<serde_json::Value, DataReadErr> {
         let cloned_path_buf = path_buf.clone();
-        let cache_key = cloned_path_buf.as_os_str();
+        let cache_key = cloned_path_buf.as_os_str().to_os_string();
         let mut data_cache = self.data_cache.lock().unwrap();
 
-        match data_cache.cache_get(cache_key) {
+        match data_cache.get(&cache_key) {
             Some(data) => {
                 debug!("data cache hit: {:?}", cache_key);
                 Ok(data.clone())
@@ -171,7 +223,13 @@ impl ServerContext {
                     match std::str::from_utf8(&data) {
                         Ok(json_str) => match serde_json::from_str::<serde_json::Value>(json_str) {
                             Ok(json) => {
-                                data_cache.cache_set(cache_key.into(), json.clone());
+                                match data_cache.insert(cache_key.clone(), json.clone()) {
+                                    Ok(_) => {}
+                                    Err(err) => error!(
+                                        "ERROR data cache insert for {:?}: {:?}",
+                                        cache_key, err
+                                    ),
+                                }
                                 Ok(json)
                             }
                             Err(err) => Err(DataReadErr::JsonError(err)),
